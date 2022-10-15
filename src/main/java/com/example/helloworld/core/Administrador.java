d56018389d726b5bd15c49e622b8ada4380675f0
Igor Mariano <igor-mariano@outlook.com>
2022-10-15 14:02:56 -0300

    Adicionando @Table(name = "Administrador")

--- src/main/java/com/example/helloworld/core/Administrador.java
+++ src/main/java/com/example/helloworld/core/Administrador.java
@@ -3,6 +3,7 @@ package com.example.helloworld.core;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 @Entity
+@Table(name = "Administrador")
 public class Administrador {
     
 }
