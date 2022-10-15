14c8b9e6922bc6aa966101a52a0e0ee5bc6488eb
Igor Mariano <igor-mariano@outlook.com>
2022-10-15 17:00:30 -0300

    Extendendo a classe DispositivoDAO

--- src/main/java/com/example/helloworld/db/DispositivoDAO.java
+++ src/main/java/com/example/helloworld/db/DispositivoDAO.java
@@ -4,6 +4,5 @@ import com.example.helloworld.core.Dispositivo;
 import io.dropwizard.hibernate.AbstractDAO;
 
 import org.hibernate.SessionFactory;
-public class DispositivoDAO {
-    
+public class DispositivoDAO extends AbstractDAO<Dispositivo>{
 }
