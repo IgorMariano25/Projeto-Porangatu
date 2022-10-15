ede120ddab9b3911d1b2b004e953c4c5ff5344a6
Igor Mariano <igor-mariano@outlook.com>
2022-10-15 13:50:24 -0300

    Adicionando import org.hibernate.SessionFactory

--- src/main/java/com/example/helloworld/db/AdministradorDAO.java
+++ src/main/java/com/example/helloworld/db/AdministradorDAO.java
@@ -3,6 +3,8 @@ package com.example.helloworld.db;
 import com.example.helloworld.core.Administrador;
 import io.dropwizard.hibernate.AbstractDAO;
 
+import org.hibernate.SessionFactory;
+
 import java.util.List;
 public class AdministradorDAO extends AbstractDAO<Administrador> {
     public Administrador create(Administrador administrador){
