1f707b72d9b53e035a42d61896072a09c2c19d2e
Igor Mariano <igor-mariano@outlook.com>
2022-10-15 17:03:11 -0300

    Realizando import java.util.List

--- src/main/java/com/example/helloworld/db/DispositivoDAO.java
+++ src/main/java/com/example/helloworld/db/DispositivoDAO.java
@@ -4,6 +4,8 @@ import com.example.helloworld.core.Dispositivo;
 import io.dropwizard.hibernate.AbstractDAO;
 
 import org.hibernate.SessionFactory;
+
+import java.util.List;
 public class DispositivoDAO extends AbstractDAO<Dispositivo>{
     public DispositivoDAO(SessionFactory factory){
         super(factory);
