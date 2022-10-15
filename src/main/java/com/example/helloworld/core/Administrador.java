7f3d59ec49e1e89937a4b9961f4e83991cf5eded
Igor Mariano <igor-mariano@outlook.com>
2022-10-15 14:11:44 -0300

    Realizando import javax.persistence.Id

--- src/main/java/com/example/helloworld/core/Administrador.java
+++ src/main/java/com/example/helloworld/core/Administrador.java
@@ -3,6 +3,8 @@ package com.example.helloworld.core;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.NamedQuery;
+import javax.persistence.Id;
+
 @Entity
 @Table(name = "Administrador")
 @NamedQuery(
