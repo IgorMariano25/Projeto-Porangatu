ac62e296d6786b0c818b0ae1e057c3014c5c9672
Igor Mariano <igor-mariano@outlook.com>
2022-10-15 13:29:45 -0300

    Realizando import do java.util.List

--- src/main/java/com/example/helloworld/db/AdministradorDAO.java
+++ src/main/java/com/example/helloworld/db/AdministradorDAO.java
@@ -1,6 +1,8 @@
 package com.example.helloworld.db;
 
 import com.example.helloworld.core.Administrador;
-import io.dropwizard.hibernate.AbstractDAO;;
+import io.dropwizard.hibernate.AbstractDAO;
+
+import java.util.List;
 public class AdministradorDAO extends AbstractDAO<Administrador> {
 }
