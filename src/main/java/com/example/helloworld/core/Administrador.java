5349a574e2a4d8d4de579f89279c66ea97ad8ca2
Igor Mariano <igor-mariano@outlook.com>
2022-10-15 14:37:00 -0300

    Realizando import javax.persistence.GenerationType

--- src/main/java/com/example/helloworld/core/Administrador.java
+++ src/main/java/com/example/helloworld/core/Administrador.java
@@ -5,6 +5,7 @@ import javax.persistence.Table;
 import javax.persistence.NamedQuery;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Column;
 
 @Entity
