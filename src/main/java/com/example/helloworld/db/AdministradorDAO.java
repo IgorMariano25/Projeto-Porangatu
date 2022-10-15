08d158875a7c0ca92376d5d2b2f301aec0eff451
Igor Mariano <igor-mariano@outlook.com>
2022-10-15 13:53:37 -0300

    Adicionando import java.util.Optional

--- src/main/java/com/example/helloworld/db/AdministradorDAO.java
+++ src/main/java/com/example/helloworld/db/AdministradorDAO.java
@@ -6,6 +6,7 @@ import io.dropwizard.hibernate.AbstractDAO;
 import org.hibernate.SessionFactory;
 
 import java.util.List;
+import java.util.Optional;
 public class AdministradorDAO extends AbstractDAO<Administrador> {
     public AdministradorDAO(SessionFactory factory) {
         super(factory);
