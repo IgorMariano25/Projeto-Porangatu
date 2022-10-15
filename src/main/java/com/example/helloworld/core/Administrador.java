1847f740b40f5988c57963ae1335ba9385f4a89b
Igor Mariano <igor-mariano@outlook.com>
2022-10-15 14:04:17 -0300

    Realizando import javax.persistence.NamedQuery

--- src/main/java/com/example/helloworld/core/Administrador.java
+++ src/main/java/com/example/helloworld/core/Administrador.java
@@ -2,6 +2,7 @@ package com.example.helloworld.core;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.NamedQuery;
 @Entity
 @Table(name = "Administrador")
 public class Administrador {
