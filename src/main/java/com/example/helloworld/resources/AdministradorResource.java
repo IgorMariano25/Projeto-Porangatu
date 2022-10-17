5370fc03b073deb749ff141ac66822ad64558529
Igor Mariano <igor-mariano@outlook.com>
2022-10-17 11:25:33 -0300

    Realizando import java.util.List

--- src/main/java/com/example/helloworld/resources/AdministradorResource.java
+++ src/main/java/com/example/helloworld/resources/AdministradorResource.java
@@ -10,6 +10,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 public class AdministradorResource {
     
 }
