68f3046c6e50a8dc75f84aa7582d686b3cba458b
Igor Mariano <igor-mariano@outlook.com>
2022-10-17 10:23:25 -0300

    Adicionando @Entity

--- src/main/java/com/example/helloworld/core/Veiculo.java
+++ src/main/java/com/example/helloworld/core/Veiculo.java
@@ -8,6 +8,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Column;
 import java.util.Objects;
+
+@Entity
 public class Veiculo {
     
 }
