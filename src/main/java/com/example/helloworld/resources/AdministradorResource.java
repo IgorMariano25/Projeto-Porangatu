dca63bb92a234d996a0cb6fff5ffef51e88b3d37
Igor Mariano <igor-mariano@outlook.com>
2022-10-17 11:19:24 -0300

    Realizando import io.dropwizard.hibernate.UnitOfWork

--- src/main/java/com/example/helloworld/resources/AdministradorResource.java
+++ src/main/java/com/example/helloworld/resources/AdministradorResource.java
@@ -2,6 +2,7 @@ package com.example.helloworld.resources;
 
 import com.example.helloworld.core.Administrador;
 import com.example.helloworld.db.AdministradorDAO;
+import io.dropwizard.hibernate.UnitOfWork;
 public class AdministradorResource {
     
 }
