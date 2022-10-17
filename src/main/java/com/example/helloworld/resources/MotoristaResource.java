3caaf1981436f6ed73895a0fd65d1c1be6514be8
Igor Mariano <igor-mariano@outlook.com>
2022-10-17 13:22:28 -0300

    Realizando import com.example.helloworld.db.MotoristaDAO;

--- src/main/java/com/example/helloworld/resources/MotoristaResource.java
+++ src/main/java/com/example/helloworld/resources/MotoristaResource.java
@@ -1,6 +1,8 @@
 package com.example.helloworld.resources;
 
 import com.example.helloworld.core.Motorista;
+import com.example.helloworld.db.MotoristaDAO;
+
 public class MotoristaResource {
     
 }
