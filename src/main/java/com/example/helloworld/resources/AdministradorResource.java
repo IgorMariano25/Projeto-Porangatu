47503883fbe89b10fcaf0719766a9de2b7720085
Igor Mariano <igor-mariano@outlook.com>
2022-10-17 11:24:09 -0300

    Realizando import javax.ws.rs.Produces

--- src/main/java/com/example/helloworld/resources/AdministradorResource.java
+++ src/main/java/com/example/helloworld/resources/AdministradorResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 public class AdministradorResource {
     
 }
