27c7945adff7ca7990181116a377daa0e9cadb25
Igor Mariano <igor-mariano@outlook.com>
2022-10-17 11:21:02 -0300

    Realizando import javax.validation.Valid

--- src/main/java/com/example/helloworld/resources/AdministradorResource.java
+++ src/main/java/com/example/helloworld/resources/AdministradorResource.java
@@ -3,6 +3,8 @@ package com.example.helloworld.resources;
 import com.example.helloworld.core.Administrador;
 import com.example.helloworld.db.AdministradorDAO;
 import io.dropwizard.hibernate.UnitOfWork;
+
+import javax.validation.Valid;
 public class AdministradorResource {
     
 }
