f69438e3d8ae1a41a987f180a59ebd7882e16e20
Igor Mariano <igor-mariano@outlook.com>
2022-10-17 10:18:53 -0300

    Realizando import javax.persistence.Id

--- src/main/java/com/example/helloworld/core/Veiculo.java
+++ src/main/java/com/example/helloworld/core/Veiculo.java
@@ -3,6 +3,7 @@ package com.example.helloworld.core;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.NamedQuery;
+import javax.persistence.Id;
 public class Veiculo {
     
 }
