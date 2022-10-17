5787ce25d29c6cc2e59ac74c39fbd91339bc3641
Igor Mariano <igor-mariano@outlook.com>
2022-10-17 10:38:18 -0300

    Realizando import org.hibernate.SessionFactory

--- src/main/java/com/example/helloworld/db/VeiculoDAO.java
+++ src/main/java/com/example/helloworld/db/VeiculoDAO.java
@@ -2,6 +2,9 @@ package com.example.helloworld.db;
 
 import com.example.helloworld.core.Veiculo;
 import io.dropwizard.hibernate.AbstractDAO;
+
+import org.hibernate.SessionFactory;
+
 public class VeiculoDAO {
     
 }
