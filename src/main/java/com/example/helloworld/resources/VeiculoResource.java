e7c65ad867104d9e08cdc7433c8ca5a7619d47ef
Igor Mariano <igor-mariano@outlook.com>
2022-10-17 13:43:49 -0300

    Realizando import javax.ws.rs.POST

--- src/main/java/com/example/helloworld/resources/VeiculoResource.java
+++ src/main/java/com/example/helloworld/resources/VeiculoResource.java
@@ -7,6 +7,7 @@ import io.dropwizard.hibernate.UnitOfWork;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.POST;
 public class VeiculoResource {
     
 }
