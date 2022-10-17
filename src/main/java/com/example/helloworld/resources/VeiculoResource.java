825fabf2674d9cce15b74ca386f96d2b4b6b1cfe
Igor Mariano <igor-mariano@outlook.com>
2022-10-17 13:45:00 -0300

    Realizando import javax.validation.Valid

--- src/main/java/com/example/helloworld/resources/VeiculoResource.java
+++ src/main/java/com/example/helloworld/resources/VeiculoResource.java
@@ -8,6 +8,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.POST;
+import javax.validation.Valid;
 public class VeiculoResource {
     
 }
