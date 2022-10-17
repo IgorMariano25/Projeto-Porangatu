fdce220e6e11af58bbf1087d94d6a29874c7a034
Igor Mariano <igor-mariano@outlook.com>
2022-10-17 13:41:44 -0300

    Realizando import javax.ws.rs.Produces

--- src/main/java/com/example/helloworld/resources/VeiculoResource.java
+++ src/main/java/com/example/helloworld/resources/VeiculoResource.java
@@ -5,6 +5,7 @@ import com.example.helloworld.db.VeiculoDAO;
 import io.dropwizard.hibernate.UnitOfWork;
 
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 public class VeiculoResource {
     
 }
