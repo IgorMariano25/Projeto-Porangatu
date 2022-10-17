cac9eccc86c948d2a6d011af8bd3f6a082348f15
Igor Mariano <igor-mariano@outlook.com>
2022-10-17 13:39:48 -0300

    Realizando import io.dropwizard.hibernate.UnitOfWork

--- src/main/java/com/example/helloworld/resources/VeiculoResource.java
+++ src/main/java/com/example/helloworld/resources/VeiculoResource.java
@@ -2,6 +2,7 @@ package com.example.helloworld.resources;
 
 import com.example.helloworld.core.Veiculo;
 import com.example.helloworld.db.VeiculoDAO;
+import io.dropwizard.hibernate.UnitOfWork;
 
 public class VeiculoResource {
     
