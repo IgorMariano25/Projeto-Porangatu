327e0a4d3645edd51983eb7071489bc726717bcf
Igor Mariano <igor-mariano@outlook.com>
2022-10-17 11:28:05 -0300

    Alterando a ordem dos imports

--- src/main/java/com/example/helloworld/resources/AdministradorResource.java
+++ src/main/java/com/example/helloworld/resources/AdministradorResource.java
@@ -4,12 +4,12 @@ import com.example.helloworld.core.Administrador;
 import com.example.helloworld.db.AdministradorDAO;
 import io.dropwizard.hibernate.UnitOfWork;
 
-import javax.validation.Valid;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.POST;
+import javax.validation.Valid;
+import javax.ws.rs.GET;
 import java.util.List;
 public class AdministradorResource {
     
