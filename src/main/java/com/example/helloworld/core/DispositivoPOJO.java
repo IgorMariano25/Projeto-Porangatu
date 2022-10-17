ffc7935e5628cddb1e083ca071da8cfec90fb756
Igor Mariano <igor-mariano@outlook.com>
2022-10-17 14:15:56 -0300

    Comentério POJO

--- src/main/java/com/example/helloworld/core/DispositivoPOJO.java
+++ src/main/java/com/example/helloworld/core/DispositivoPOJO.java
@@ -1,5 +1,11 @@
 package com.example.helloworld.core;
 
+/*
+ POJO - Não extende de outra classe
+ POJO - Não implementa interfaces
+ POJO - Não tem notações externas
+ */
+
 public class DispositivoPOJO {
     
 }
