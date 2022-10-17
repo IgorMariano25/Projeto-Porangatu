e2406f3d2c0f58c2a47d3fb8ecdfe2a9d357b62a
Igor Mariano <igor-mariano@outlook.com>
2022-10-17 11:21:50 -0300

    Realizando import javax.ws.rs.GET

--- src/main/java/com/example/helloworld/resources/AdministradorResource.java
+++ src/main/java/com/example/helloworld/resources/AdministradorResource.java
@@ -5,6 +5,7 @@ import com.example.helloworld.db.AdministradorDAO;
 import io.dropwizard.hibernate.UnitOfWork;
 
 import javax.validation.Valid;
+import javax.ws.rs.GET;
 public class AdministradorResource {
     
 }
