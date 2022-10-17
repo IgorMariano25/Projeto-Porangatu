7ee53b800b3eca91a6571d1d062bd599ae60c308
Igor Mariano <igor-mariano@outlook.com>
2022-10-17 13:22:48 -0300

    Realizando import io.dropwizard.hibernate.UnitOfWork

--- src/main/java/com/example/helloworld/resources/MotoristaResource.java
+++ src/main/java/com/example/helloworld/resources/MotoristaResource.java
@@ -2,6 +2,7 @@ package com.example.helloworld.resources;
 
 import com.example.helloworld.core.Motorista;
 import com.example.helloworld.db.MotoristaDAO;
+import io.dropwizard.hibernate.UnitOfWork;
 
 public class MotoristaResource {
     
