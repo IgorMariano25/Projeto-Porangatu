5752b60232dc849a9c42137d5b25398033d4f5bb
Igor Mariano <igor-mariano@outlook.com>
2022-10-17 11:18:48 -0300

    Realizando import com.example.helloworld.db.AdministradorDAO

--- src/main/java/com/example/helloworld/resources/AdministradorResource.java
+++ src/main/java/com/example/helloworld/resources/AdministradorResource.java
@@ -1,6 +1,7 @@
 package com.example.helloworld.resources;
 
 import com.example.helloworld.core.Administrador;
+import com.example.helloworld.db.AdministradorDAO;
 public class AdministradorResource {
     
 }
