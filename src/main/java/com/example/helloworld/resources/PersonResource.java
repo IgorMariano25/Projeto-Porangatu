66857ae10d0dd21473ab682144903442af5a09a2
Igor Mariano <igor-mariano@outlook.com>
2022-11-11 20:20:46 -0300

    Realizando import javax.ws.rs.PUT

--- src/main/java/com/example/helloworld/resources/PersonResource.java
+++ src/main/java/com/example/helloworld/resources/PersonResource.java
@@ -9,6 +9,7 @@ import javax.ws.rs.BadRequestException;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
