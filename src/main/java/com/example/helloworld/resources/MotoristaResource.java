873798f507eb9ad2fe005f6630e75fd35ca17165
Igor Mariano <igor-mariano@outlook.com>
2022-11-11 20:20:12 -0300

    Realizando import javax.ws.rs.PUT

--- src/main/java/com/example/helloworld/resources/MotoristaResource.java
+++ src/main/java/com/example/helloworld/resources/MotoristaResource.java
@@ -8,6 +8,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.validation.Valid;
 import javax.ws.rs.GET;
 import java.util.List;
